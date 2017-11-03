d7545d35d06e17d2a87f8516904a7c93e92ff643
Vladimir Makhlay <v.makhlay@itgene.ru>
2017-11-03 15:45:18 +0300

    Adds static and media files settings to the example app

--- example/example/settings.py
+++ example/example/settings.py
@@ -152,6 +152,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+MEDIA_URL = "/media/"
 
 CMS_TEMPLATES = [
     ('base.html', 'Base page template'),
